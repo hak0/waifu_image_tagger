77d62566e3ef02ebab350cda759c1b5e6132bc7f
hak0 <lx.combox@gmail.com>
2019-11-08 11:20:43 -0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+extern crate rustnao;
+use rustnao::HandlerBuilder;
+
+fn test_rustnao() {
+    let handle = HandlerBuilder::new()
+        .api_key("your_api_key")
+        .num_results(999)
+        .db(999)
+        .build();
+    let result = handle.get_sauce("./tests/test2.jpg", None, None);
+    println!("{:?}", result);
+}
+
+fn main() {
+    test_rustnao();
+    // Use a vec to save
+    println!("Hello, world!");
+}
