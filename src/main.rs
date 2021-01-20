85480f6c67076492ce3638ef9fed19bd3430d42e
hak0 <lx.combox@gmail.com>
2021-01-20 10:26:08 +0800

    bug fix

--- src/main.rs
+++ src/main.rs
@@ -177,8 +177,7 @@ fn tag_single_image(
                         "https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&id={}",
                         gelbooru_id
                     );
-                    match serde_json::from_str::<serde_json::Value>(&reqwest::blocking::get(&json_url)?
-                        .text()?)
+                    match &reqwest::blocking::get(&json_url)?.json::<serde_json::Value>()
                     {
                         Ok(v) => v[0]["tags"]
                             .to_string()
